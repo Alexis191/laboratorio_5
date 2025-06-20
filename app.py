6a1e872b5bc3868a4a092f339d526ebf2363df3a
Alexis <acollaguazoa1@est.ups.edu.ec>
2025-06-19 19:53:53 -0500

    Actualización de la función exportar resumen

--- app.py
+++ app.py
@@ -16,8 +16,8 @@ libro.exportar_resumen("resumen_contable.txt")
 
 #Probar código sin archivo csv
 #libro.agregar_transaccion('2025-06-19', 'Venta de disco SSD', 80, 'InGrEso') 
-#libro.agregar_transaccion('2025-06-18', 'Compra de teclado Polaroid', 50, 'EGreSO')  
-#libro.agregar_transaccion('2025-06-19', 'Venta de pantalla ENV', 200, 'ingreso') 
-#libro.agregar_transaccion('2025-06-16', 'Compra de mouse Logitech', 100, 'egreso')  
-# print(libro.calcular_resumen())
+#libro.agregar_transaccion('2025-06-18', 'Compra de teclado Polaroid', -50, 'EGreSO')  
+#libro.agregar_transaccion('19-06-2025', 'Venta de pantalla ENV', 200, 'ingreso') 
+#libro.agregar_transaccion('2025-06-16', 'Compra de mouse Logitech', 100, 'gasto')  
+#print(libro.calcular_resumen())
 
